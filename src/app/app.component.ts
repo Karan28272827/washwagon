bfa5f406f81f2e4df30a8746e08041eb2bba8a27
Mulaydm10 <dhruvmulay10@gmail.com>
2024-12-01 18:58:52 +0530

    intial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
+import { NavbarComponent } from './routes/navbar/navbar.component';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './routes/footer/footer.component';
 import { LandingPageComponent } from './routes/landing-page/landing-page.component';
 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,FooterComponent,LandingPageComponent],
+  imports: [RouterOutlet,FooterComponent,LandingPageComponent,NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
