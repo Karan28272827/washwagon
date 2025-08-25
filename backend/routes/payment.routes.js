589384a2754bbc51d967e91395267c08052f03b2
Akhil9n <akhilesh.nawale20@vit.edu>
2025-08-25 22:41:42 +0530

    Delete backend directory

--- backend/routes/payment.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { processPayment } = require('../controller/payment.controller');
-
-const router = express.Router();
-
-router.post('/process', processPayment);
-
-module.exports = router;
